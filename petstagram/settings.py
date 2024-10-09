42627216c590bf93e02c5b2578bed2586da34953
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-10-09 21:24:51 +0300

    Added new features to petstagram

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-no!wi$b+dw!m$z3h^6e@*j+8^^pajmlui5hc%!(0nhm!d=x8es
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = []
 
 
 # Application definition
