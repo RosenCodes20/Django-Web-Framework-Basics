221f0a30d39de404a2a90f7c912484f8fb618513
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-10-20 02:15:10 +0300

    Upgraded the views and urls files.

--- exam_preparation_3/exam_preparation_3/tasty_recipes/views.py
+++ exam_preparation_3/exam_preparation_3/tasty_recipes/views.py
@@ -4,3 +4,48 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, "home-page.html")
+
+
+def catalogue(request):
+
+    return render(request, "catalogue.html")
+
+
+def create_profile(request):
+
+    return render(request, "create-profile.html")
+
+
+def create_recipe(request):
+
+    return render(request, "create-recipe.html")
+
+
+def delete_profile(request):
+
+    return render(request, "delete-profile.html")
+
+
+def delete_recipe(request, pk):
+
+    return render(request, "delete-recipe.html")
+
+
+def profile_details(request):
+
+    return render(request, "delete-profile.html")
+
+
+def recipe_details(request, pk):
+
+    return render(request, "details-recipe.html")
+
+
+def edit_profile(request):
+
+    return render(request, "edit-profile.html")
+
+
+def edit_recipe(request, pk):
+
+    return render(request, "edit-recipe.html")
