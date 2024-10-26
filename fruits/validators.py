e1f170e4e1e430afb5a5d52172318fdb6fe9e61e
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-10-26 18:33:43 +0300

    Delete fruits directory

--- fruits/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_fruit_name(value):
-
-    if not value.isalpha():
-        raise ValidationError("Fruit name should contain only letters!")
