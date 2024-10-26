02df7bed59ea4c57466eef3926cc8ab39c5ff0ec
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-10-26 18:33:48 +0300

    Delete profiles directory

--- profiles/templatetags/get_profiles.py
+++
@@ -1,9 +0,0 @@
-from django.template import Library
-
-from profiles.models import Profile
-
-register = Library()
-
-@register.simple_tag
-def get_profiles():
-    return Profile.objects.all()
--- profiles/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_profile_first_name(value):
-
-    if not value[0].isalpha():
-        raise ValidationError("Your name must start with a letter!")
